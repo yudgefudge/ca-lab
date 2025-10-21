d763af179f1323850d394d30c15ab414128e866c
Nubcoder0118 <brad.hu.2006@gmail.com>
2025-10-21 12:08:41 -0400

    fixed this class

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -1,5 +1,7 @@
 package use_case.logout;
 
+import entity.User;
+
 /**
  * The Logout Interactor.
  */
@@ -9,15 +11,18 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
     }
 
     @Override
     public void execute() {
-        // TODO: implement the logic of the Logout Use Case
         // * set the current username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         // * tell the presenter to prepare a success view.
+        userDataAccessObject.setCurrentUsername(null);
+        final LogoutOutputData logOutputData = new LogoutOutputData(null);
+        logoutPresenter.prepareSuccessView(logOutputData);
     }
 }
 
